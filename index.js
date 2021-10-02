7216cc1e14ff42ac9c3fa259352060906a02c58e
mansch404 <mansch404@gmail.com>
2021-10-02 11:38:43 +0200

    Added Post

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: "Meditating with MJA" },
+    { id: 2, name: "Morning meditation" },
+    { id: 3, name: "course3" }
+];
+
+// ------GET------
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/api/meditationcourses', (req, res) => {
-    res.send(JSON.parse('{"data" : [1, 2, 3, 4, 5, "hallo"]}'));
+    res.send(courses);
 });
 
-app.get('/api/meditationcourses/:id/:name', (req, res) => {
-    res.send(req.query);
+app.get('/api/meditationcourses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send("404 - The course with the given id does not exist.")
+    res.send(course)
 });
+
+// ---------------
+
+
+// ------POST------
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+// ----------------
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
