cb18b6c1a25d6db28bd1e73db4c679d504203a2c
mansch404 <mansch404@gmail.com>
2021-10-03 21:04:12 +0200

    Added put

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -36,6 +37,12 @@ app.get('/api/meditationcourses/:id', (req, res) => {
 // ------POST------
 
 app.post('/api/meditationcourses', (req, res) => {
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -45,6 +52,37 @@ app.post('/api/meditationcourses', (req, res) => {
 });
 
 // ----------------
+
+
+// ------PUT------
+
+app.put('/api/meditationcourses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send("404 - The course with the given id does not exist.")
+    
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course)
+});
+
+// ---------------
+
+
+// ------FUNCTIONS------
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.validate(course, schema);
+}
+
+// ---------------------
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
